c0ff8079c1a6f874dbc23f56d9569e971ce7ae83
Yentis <yentl178@gmail.com>
2018-03-22 13:58:49 +0100

    changed password auth to unmodified

---
+++ StringEncrypt.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Startup
+{
+    private static char[] hexdic = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+
+    public async Task<object> Invoke(object input)
+    {
+        return Hashing((String)input);
+    }
+
+    public static string Decrypt(string crypt, string keyString, bool forceDecryptWithoutPrefix)
+    {
+        if (!crypt.StartsWith("crypt::") && !forceDecryptWithoutPrefix)
+        {
+            return crypt;
+        }
+        string text = crypt;
+        if (!forceDecryptWithoutPrefix)
+        {
+            text = crypt.Substring(7);
+        }
+        Random random = new Random((keyString.GetHashCode() + 126991) % 1048576);
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < text.Length; i += 2)
+        {
+            char c = text[i];
+            char c2 = text[i + 1];
+            int num;
+            if (c >= 'A')
+            {
+                num = (int)(c - 'A' + '\n');
+            }
+            else
+            {
+                num = (int)(c - '0');
+            }
+            int num2;
+            if (c2 >= 'A')
+            {
+                num2 = (int)(c2 - 'A' + '\n');
+            }
+            else
+            {
+                num2 = (int)(c2 - '0');
+            }
+            int num3 = num * 16 + num2;
+            num3 = (num3 + 128 - random.Next(128)) % 128;
+            stringBuilder.Append((char)num3);
+        }
+        return stringBuilder.ToString();
+    }
+
+    public static string Encrypt(string plaintext, string keyString, bool ignorePrefix)
+    {
+        StringBuilder stringBuilder;
+        if (ignorePrefix)
+        {
+            stringBuilder = new StringBuilder("");
+        }
+        else
+        {
+            stringBuilder = new StringBuilder("crypt::");
+        }
+        Random random = new Random((keyString.GetHashCode() + 126991) % 1048576);
+        for (int i = 0; i < plaintext.Length; i++)
+        {
+            int num = (int)plaintext[i];
+            num = (num + random.Next(128)) % 128;
+            stringBuilder.Append(hexdic[num / 16]);
+            stringBuilder.Append(hexdic[num % 16]);
+        }
+        return stringBuilder.ToString();
+    }
+
+    public static string Hashing(string plaintext)
+    {
+        SHA512 sha = new SHA512Managed();
+        byte[] array = sha.ComputeHash(Encoding.ASCII.GetBytes(plaintext));
+        StringBuilder stringBuilder = new StringBuilder();
+        foreach (byte b in array)
+        {
+            stringBuilder.AppendFormat("{0:x2}", b);
+        }
+        return stringBuilder.ToString();
+    }
+}
--- app.js
+++ app.js
@@ -1,18 +1,165 @@
+/* jshint node: true */
+/* jshint esnext: true */
+'use strict';
+
 var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 var sql = require('mssql');
 var async = require('async');
 var fs = require('fs');
+var edge = require('edge');
+var bcrypt = require('bcrypt');
 var itemNames;
+var StringEncrypt = edge.func(require('path').join(__dirname, 'StringEncrypt.cs'));
+const PORT = 8080;
 
 var config = {
     user: 'sa',
-    password: 'PK8VSZ2HyHJAbNJ6Kb4R54fnqNu2Jd5K',
-    server: 'YENTL-PC',
+    password: '',
+    server: '',
     database: 'heroes'
 };
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cookieParser());
+
+app.get('/login', home);
+
+app.get('*', function (req, res) {
+    res.redirect('/login');
+});
+
+app.post('/login', processLogin, home);
+
+app.post('/itempicker', function(req, res) {
+    var charname = req.body.charname;
+
+    getCharID(charname, function (charID) {
+        buildItemList(charID, function (itemsUpdated) {
+            res.render('itempicker', {
+                items: itemsUpdated,
+                charID: charID,
+                error: ""
+            });
+        });
+    });
+});
+
+app.post('/colorpicker', function(req, res) {
+    var itemID = req.body.itemname;
+    var charID = req.body.charID;
+
+    var color1 = req.body.color1,
+        color2 = req.body.color2,
+        color3 = req.body.color3;
+
+    var dec1 = parseInt(color1.substring(1), 16);
+    var dec2 = parseInt(color2.substring(1), 16);
+    var dec3 = parseInt(color3.substring(1), 16);
+
+    runDBQuery(itemID, dec1, dec2, dec3, function () {
+        buildItemList(charID, function (itemsUpdated) {
+            res.render('itempicker', {
+                items: itemsUpdated,
+                charID: charID,
+                error: "Item successfully dyed."
+            });
+        });
+    });
+});
+
+fs.readFile('itemnames.json', 'utf8', function (err, data) {
+    if (err) {throw err;}
+    itemNames = JSON.parse(data);
+    console.log(itemNames);
+    console.log('Server is running..');
+});
+
+app.listen(PORT, function () {
+});
+
+function home(req, res){
+    var context = req.dataProcessed;
+
+    res.render('login', {
+        post: "login",
+        curpage: "Dye System",
+        error: context
+    });
+}
+
+function processLogin(req, res, next){
+    var accname = req.body.accname;
+    var password = req.body.password;
+
+    checkPassword(accname, password, function (result) {
+        if(result){
+            getAccID(accname, function (accID) {
+                getCharnames(accID, function (characters) {
+                    res.render('charpicker', {
+                        characters: characters,
+                        post: "itempicker"
+                    });
+                });
+            });
+        } else if (result === null) {
+            req.dataProcessed = 'Invalid account name, please try again.';
+            return next();
+        } else {
+            req.dataProcessed = 'Invalid password, please try again.';
+            return next();
+        }
+    });
+}
+
+function checkPassword(accname, password, callback){
+    if(accname.length > 24){
+        accname = accname.substring(0, 24);
+    }
+
+    StringEncrypt(password, function (error, result) {
+        var hashedpass = result;
+
+        var conn = new sql.ConnectionPool(config);
+        conn.connect()
+            .then(function () {
+                var req = new sql.Request(conn);
+                req.input('accountName', sql.VarChar, accname);
+                req.query('select * from "User" where Name = @accountName')
+                    .then(function (result) {
+                        var storedhash = result.recordset[0][Object.keys(result.recordset[0])[6]];
+                        conn.close();
+
+                        if(storedhash === hashedpass){
+                            callback(true);
+                        } else {
+                            callback(false);
+                        }
+                    })
+                    .catch(function () {
+                        conn.close();
+                        callback(null);
+                    });
+            })
+            .catch(function (callback, err) {
+                console.log("Connection error: " + err);
+                callback(null);
+            });
+        conn.on('error', err => {
+            console.log("An error occurred: " + err);
+            callback(null);
+        });
+    });
+}
 
 function getAccID(accname, callback){
     var conn = new sql.ConnectionPool(config);
@@ -21,13 +168,41 @@ function getAccID(accname, callback){
             var req = new sql.Request(conn);
             req.input('accountName', sql.VarChar, accname);
             req.query('select * from "User" where Name = @accountName')
-                .then(function (result, err) {
+                .then(function (result) {
                     var accID = result.recordset[0][Object.keys(result.recordset[0])[0]];
                     conn.close();
                     callback(accID);
                 })
                 .catch(function (err) {
                     console.log("Query error: " + err);
+                    conn.close();
+                    callback(null);
+                });
+        })
+        .catch(function (callback, err) {
+            console.log("Connection error: " + err);
+            callback(null);
+        });
+    conn.on('error', err => {
+        console.log("An error occurred: " + err);
+        callback(null);
+    });
+}
+
+function getCharAcc(charname, callback){
+    var conn = new sql.ConnectionPool(config);
+    conn.connect()
+        .then(function () {
+            var req = new sql.Request(conn);
+            req.input('charname', sql.VarChar, charname);
+            req.query('SELECT * FROM CharacterInfo WHERE Name = @charname')
+                .then(function (result) {
+                    var accID = result.recordset[0][Object.keys(result.recordset[0])[1]];
+                    conn.close();
+                    callback(accID);
+                })
+                .catch(function (err) {
+                    console.log("Query error getCharAcc: " + err);
                     conn.close();
                     callback(null);
                 });
@@ -49,7 +224,7 @@ function getCharnames(accID, callback){
             var req = new sql.Request(conn);
             req.input('accountID', sql.VarChar, accID);
             req.query('SELECT * FROM CharacterInfo WHERE UID = @accountID')
-                .then(function (result, err) {
+                .then(function (result) {
                     let characters = [];
                     for(var i = 0; i < Object.keys(result.recordset).length; i++){
                         characters.push({name: result.recordset[i][Object.keys(result.recordset[i])[3]]});
@@ -80,7 +255,7 @@ function getCharID(charname, callback){
             var req = new sql.Request(conn);
             req.input('charname', sql.NVarChar, charname);
             req.query("select * from CharacterInfo where Name = @charname")
-                .then(function (result, err) {
+                .then(function (result) {
                     var charID = result.recordset[0][Object.keys(result.recordset[0])[0]];
                     conn.close();
                     callback(charID);
@@ -108,7 +283,7 @@ function getItemIDs(charID, callback){
             var req = new sql.Request(conn);
             req.input('characterID', sql.VarChar, charID);
             req.query('SELECT * FROM Item WHERE EXISTS (SELECT * FROM Equippable WHERE Item.ID = Equippable.ID)  AND OwnerID = @characterID')
-                .then(function (result, err) {
+                .then(function (result) {
                     let items = [];
                     for(var i = 0; i < Object.keys(result.recordset).length; i++){
                         items.push({
@@ -142,7 +317,7 @@ function getItemColors(itemID, callback){
             var req = new sql.Request(conn);
             req.input('itemID', sql.VarChar, itemID);
             req.query('SELECT * FROM Equippable WHERE ID = @itemID')
-                .then(function (result, err) {
+                .then(function (result) {
                     let colors = [];
                     for(var i = 0; i < Object.keys(result.recordset).length; i++){
                         colors.push({
@@ -180,7 +355,7 @@ function runDBQuery(itemID, dec1, dec2, dec3, callback){
         .then(function () {
             var req = new sql.Request(conn);
             req.query(queryString)
-                .then(function (result, err) {
+                .then(function () {
                     conn.close();
                     callback();
                 })
@@ -216,7 +391,7 @@ function buildItemList(charID, callback){
                 if(itemNames[item.name.toUpperCase()] === undefined){
                     name = item.name;
                 } else {
-                    name = itemNames[item.name.toUpperCase()]
+                    name = itemNames[item.name.toUpperCase()];
                 }
 
                 itemsUpdated.push({
@@ -229,7 +404,7 @@ function buildItemList(charID, callback){
 
                 callback();
             });
-        }, function (err) {
+        }, function () {
             callback(itemsUpdated);
         });
     });
@@ -241,80 +416,3 @@ function sortByKey(array, key) {
         return ((x < y) ? -1 : ((x > y) ? 1 : 0));
     });
 }
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json());
-app.use(express.urlencoded());
-
-app.get('/', function(req, res) {
-    res.render('index');
-});
-
-app.post('/', function(req, res) {
-    var accname = req.body.accname;
-
-    getAccID(accname, function (accID) {
-        if(accID === null){
-            res.render('index', {
-                error: 'Invalid account name, please try again.'
-            })
-        } else {
-            getCharnames(accID, function (characters) {
-                res.render('charpicker', {
-                    characters: characters
-                });
-            });
-        }
-    });
-});
-
-app.post('/itempicker', function(req, res) {
-    var charname = req.body.charname;
-
-    getCharID(charname, function (charID) {
-        buildItemList(charID, function (itemsUpdated) {
-            res.render('itempicker', {
-                items: itemsUpdated,
-                charID: charID,
-                error: ""
-            })
-        });
-    });
-});
-
-app.post('/colorpicker', function(req, res) {
-    var itemID = req.body.itemname;
-    var charID = req.body.charID;
-
-    var color1 = req.body.color1,
-        color2 = req.body.color2,
-        color3 = req.body.color3;
-
-    var dec1 = parseInt(color1.substring(1), 16);
-    var dec2 = parseInt(color2.substring(1), 16);
-    var dec3 = parseInt(color3.substring(1), 16);
-
-    runDBQuery(itemID, dec1, dec2, dec3, function () {
-        buildItemList(charID, function (itemsUpdated) {
-            res.render('itempicker', {
-                items: itemsUpdated,
-                charID: charID,
-                error: "Item successfully dyed"
-            })
-        });
-    });
-});
-
-fs.readFile('itemnames.json', 'utf8', function (err, data) {
-    if (err) throw err;
-    itemNames = JSON.parse(data);
-    console.log(itemNames);
-    console.log('Server is running..');
-});
-
-var server = app.listen(5000, function () {
-});
